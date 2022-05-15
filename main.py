1df2ef462e66422bed49862ae73a0e246436291c
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-05-15 11:02:09 -0400

    Part 3: voice channel join y leave
    - se elimina el intento de subir un chiste con jokeurl.
    
    -cambio del prefix de t! a w!

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aplkeys import *
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = 't!', intents=intents)
+client = commands.Bot(command_prefix = 'w!', intents=intents)
 
 
 @client.event
@@ -23,28 +23,26 @@ async def hello(ctx):
 @client.event
 async def on_member_join(member):
 
-    jokeurl = "https://joke3.p.rapidapi.com/v1/joke"
-
-	payload = {
-	"content": "A joke here",
-	"nsfw": "false"
-	}
-	headers = {
-		"content-type": "application/json",
-		"X-RapidAPI-Host": "joke3.p.rapidapi.com",
-		"X-RapidAPI-Key": JOKEAPI
-	}
-
-	response = requests.request("POST", jokeurl, json=payload, headers=headers)
-
-	channel = client.get_channel(954536854273794068)
 	await channel.send('Welcome, to server')
-	await channel.send(json.loads(response.text)['content'])
-
+	
 @client.event
 async def on_member_remove(member):
 	channel = client.get_channel(954536854273794068)
 	await channel.send('GoodBye')
 
+@client.command(pass_context = True)
+async def join(ctx):
+	if (ctx.author.voice):
+		channel = ctx.message.author.voice.channel
+		await channel.connect()
+	else:
+		await ctx.send('No estas en un canal de voz, debes estar en uno para usar este comando')
+
+@client.command(pass_context = True)
+async def leave(ctx):
+	if (ctx.voice_client):
+		await ctx.guild.voice_client.disconnect() 
+	else:
+		await ctx.send('No estoy en ningún canal de voz')
 
 client.run(BOTTOKEN)

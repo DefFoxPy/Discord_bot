6de35e457e26950846890267781c863e5dd26681
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-07 19:24:24 -0400

    Making a Discord bot #3
    commandos
    *añadir alias a un comando
    * permitir una entrada con muchos parametros

--- bot_civo.py
+++ bot_civo.py
@@ -1,4 +1,4 @@
-import discord, os
+import discord, os, random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,24 +11,15 @@ bot = commands.Bot(command_prefix='z/', intents=discord.Intents.all())
 async def on_ready():
 	print('Bot is online')
 
-@bot.event
-async def on_message(message):
-	if message.content.lower() == 'hello':
-		await message.channel.send('Hey!')
-
-@bot.event
-async def on_member_join(member):
-	channel = member.guild.system_channel
-	await channel.send(f'{member.mention} Welcomen to the Server.')
-
-@bot.event
-async def on_member_remove(member):
-	channel = member.guild.system_channel
-	await channel.send(f'Goodbye {member.mention}')
-
 @bot.command()
 async def ping(ctx):
-	await ctx.reply('Pong')
+	await ctx.reply(f'Pong! {round(bot.latency * 1000)}ms')
+
+@bot.command(aliases=['8ball', 'test']) # se puede agregar alias al comando
+async def eightball(ctx, *, question):  # permite que el parametro tenga muchos espacios
+	responses = ['yes', 'no', 'maybe']
+	await ctx.send(f'**Question: **{question}\n**Answers: **{random.choice(responses)}')
+
 
 
 bot.run(TOKEN)

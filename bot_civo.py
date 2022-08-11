521805ddac947563f40612f766134e4e80ebf8c6
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-11 19:43:21 -0400

    Making a discord bot API #8
    se llaman a una página web

--- bot_civo.py
+++ bot_civo.py
@@ -1,4 +1,5 @@
 import discord, os, random
+import requests
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER_ID = os.getenv('SERVER_ID')
 
+
 bot = commands.Bot(command_prefix='z/', intents=discord.Intents.all())
 
 @bot.event
@@ -14,19 +16,23 @@ async def on_ready():
 	print('Bot is online')
 
 @bot.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member:discord.Member, *, reason=None):
-	if reason == None:
-		reason = "No reason provided"
-	await ctx.guild.kick(member)  
-	await ctx.send(f'**User: **{member.mention} has been kicked for: *{reason}*')
+async def dog(ctx):
+	r = requests.get("https://dog.ceo/api/breeds/image/random")
+	res = r.json()
+	embed = discord.Embed()
+	embed.set_image(url=res['message'])
+	await ctx.send(embed=embed)
 
 @bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member:discord.Member, *, reason=None):
-	if reason == None:
-		reason = "No reason provided"
-	await ctx.guild.ban(member)  
-	await ctx.send(f'**User: **{member.mention} has been Banned for: *{reason}*')
+async def wyr(ctx):
+	r = requests.get("https://api.truthordarebot.xyz/v1/wyr")
+	res = r.json()
+	await ctx.send(res['question'])
 
+@bot.command()
+async def truth(ctx):
+	r = requests.get("https://api.truthordarebot.xyz/v1/truth")
+	res = r.json()
+	await ctx.send(res['question'])
+	
 bot.run(TOKEN)

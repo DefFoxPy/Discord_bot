d5551129ad3156841f6651dad66cc75b347b66ca
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-11 19:23:33 -0400

    Making a Discord bot #6 status

--- bot_civo.py
+++ bot_civo.py
@@ -11,28 +11,7 @@ bot = commands.Bot(command_prefix='z/', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
+	await bot.change_presence(status=discord.Status.dnd, activity=discord.Game("Discord.py"))
 	print('Bot is online')
-
-class MyBot(discord.Client):
-	def __init__(self):
-		super().__init__(intents=discord.Intents.default())
-		self.synced = False
-
-	async def on_ready(self):
-		await tree.sync(guild=discord.Object(id=SERVER_ID))
-		self.synced = True
-		print('Bot is online')
-
-bot = MyBot()
-tree = app_commands.CommandTree(bot)
-
-@tree.command(name='ping', description='Pings the user', guild=discord.Object(id=SERVER_ID))
-async def self(interaction:discord.Interaction):
-	await interaction.response.send_message(f'Pong')
-
-@tree.command(name='eightball', description='gives your a answer', guild=discord.Object(id=SERVER_ID))
-async def self(interaction:discord.Interaction, question:str):
-	responses = ['yes', 'no', 'maybe']
-	await interaction.response.send_message(f'**Question: **{question}\n**Answers: **{random.choice(responses)}')	
 
 bot.run(TOKEN)

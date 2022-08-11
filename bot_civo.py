86135294b04b8fe87024cd7e5d333d7d2776b6ad
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-11 19:31:49 -0400

    Making a Discord bot #7 kick/ban
    "" ,* "" usado para aceptar una entrada de varias lineas

--- bot_civo.py
+++ bot_civo.py
@@ -11,7 +11,22 @@ bot = commands.Bot(command_prefix='z/', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-	await bot.change_presence(status=discord.Status.dnd, activity=discord.Game("Discord.py"))
 	print('Bot is online')
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member:discord.Member, *, reason=None):
+	if reason == None:
+		reason = "No reason provided"
+	await ctx.guild.kick(member)  
+	await ctx.send(f'**User: **{member.mention} has been kicked for: *{reason}*')
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member:discord.Member, *, reason=None):
+	if reason == None:
+		reason = "No reason provided"
+	await ctx.guild.ban(member)  
+	await ctx.send(f'**User: **{member.mention} has been Banned for: *{reason}*')
 
 bot.run(TOKEN)

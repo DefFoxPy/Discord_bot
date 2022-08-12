8bfba7cbdc252e2db97f11c3e9066912748ef54c
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-11 21:02:31 -0400

    Making a discord game cogs #10

--- bot_civo.py
+++ bot_civo.py
@@ -1,4 +1,5 @@
 import discord, os
+import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -8,25 +9,13 @@ SERVER_ID = os.getenv('SERVER_ID')
 
 bot = commands.Bot(command_prefix='z/', intents=discord.Intents.all(), help_command=None)
 
-@bot.event
-async def on_ready():
-	print('Bot is online')
+async def load():
+	for filename in os.listdir('./cogs'):
+		if filename.endswith('.py'):
+			await bot.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.group(name="help", invoke_without_command=True) #impide que se vuelva a llamar help en otro comando del grupo
-async def help(ctx):
-	await ctx.send('Games\nUtilitys\nEcosystem')
+async def main():
+	await load()
+	await bot.start(TOKEN)
 
-@help.command()
-async def game(ctx):
-	await ctx.send("These are the game comands 'rps'")
-
-@help.command()
-async def utilitys(ctx):
-	await ctx.send("These are the utilitys comands")
-
-@help.command()
-async def eco(ctx):
-	await ctx.send("These are the eco comands")
-
-
-bot.run(TOKEN)
+asyncio.run(main())

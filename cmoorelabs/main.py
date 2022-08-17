e06740432fb0374acb4b0a226ba8111548817961
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-17 13:21:37 -0400

    feat: Part 2 by cmoorelabs
    adding commands and working with API

--- cmoorelabs/main.py
+++ cmoorelabs/main.py
@@ -1,29 +1,45 @@
 import discord
 import asyncio
 import config
+import requests
+
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='z!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('logged on as', client.user)
+    print('logged on as', bot.user)
 
-@client.event
+@bot.event
 async def on_message(message):
+    # Primero procese los eventos
+    await bot.process_commands(message) 
+
+    # don't respond to command_prefix
+    if message[0:2] == 'z!':
+        return
+
     # don't respond to ourselves
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
     if message.content == 'ping':
         await message.channel.send('Pong.')
+
+@bot.command()
+async def facts(ctx, number):
+    ''' Usando una API para obtener datos curiosos sobre los números '''
+    response = requests.get(f'http://numbersapi.com/{number}')
+    await ctx.channel.send(response.text)
 
 async def setup():
     print('Setting up...')
 
 async def main():
     await setup()
-    await client.start(config.TOKEN)
+    await bot.start(config.TOKEN)
 
 asyncio.run(main())

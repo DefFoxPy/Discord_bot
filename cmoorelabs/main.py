2af72b9294917948cff6f8df4dc98f7c2bb89ed9
juanmpy05 <66576421+juanmpy05@users.noreply.github.com>
2022-08-22 22:04:47 -0400

    refactor: Update main.py
    Now the commands work when the bot is called

--- cmoorelabs/main.py
+++ cmoorelabs/main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='z!', intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('z!'), intents=intents)
 
 @bot.event
 async def on_ready():
